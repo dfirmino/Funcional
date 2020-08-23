a63e8c8bdd8a20aee44b50132f77b58aa964bae6
Davi Firmino <dafirsouza@gmail.com>
2020-08-23 13:50:09 -0300

    separando linhas

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ const caminho = path.join(__dirname, 'Game_of_Thrones_season1');
 fn.lerDiretorio(caminho)
     .then(arquivos => fn.elementosTerminadosCom(arquivos, '.srt'))
     .then(caminhos => fn.lerArquivos(caminhos))
+    .then(conteudos => conteudos.join(''))
+    .then(todoConteudo => todoConteudo.split('\n'))
     .then(console.log)
     .catch(console.log)
